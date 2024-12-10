51f6c709c4d52aed3988f689cf06b38d757eff6b
SamTang1303 <samtang1303@gmail.com>
2024-12-10 15:17:12 -0800

    Day 4 part 1 solution

--- day4/solution.go
+++ day4/solution.go
@@ -3,40 +3,70 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 )
 
-func parseInput(filePath string) ([][]int, error) {
+func parseInput(filePath string) []string {
 	input, err := os.ReadFile(filePath)
 	if err != nil {
-		return [][]int{}, fmt.Errorf("Problem reading file")
+		return []string{}
 	}
-	inputStr := string(input)
-	lines := strings.Split(inputStr, "\n")
-	var data [][]int
-	for _, val := range lines {
-		numsStr := strings.Fields(val)
-		nums_int := make([]int, len(numsStr))
-		if len(numsStr) == 0 {
-			continue
-		}
-		for i := range numsStr {
-			nums_int[i], err = strconv.Atoi(numsStr[i])
-			if err != nil {
-				fmt.Print("Error converting string")
-				break
-			}
-		}
-		data = append(data, nums_int)
+	lines := strings.Split(string(input), "\n")
+	return lines[:len(lines)-1]
+}
+
+var xmasPosition = map[byte]int{
+	'X': 0,
+	'M': 1,
+	'A': 2,
+	'S': 3,
+}
+
+var directions = [8][2]int{
+	{1, 1},
+	{0, 1},
+	{1, 0},
+	{-1, 0},
+	{0, -1},
+	{-1, 1},
+	{1, -1},
+	{-1, -1},
+}
+
+func validSubstr(direction [2]int, position [2]int, puzzle *[]string) bool {
+	letter := (*puzzle)[position[1]][position[0]]
+	xOutOfBounds := position[0]+direction[0] > len((*puzzle)[0])-1 || position[0]+direction[0] < 0
+	yOutOfBounds := position[1]+direction[1] > len(*puzzle)-1 || position[1]+direction[1] < 0
+
+	if xOutOfBounds || yOutOfBounds {
+		return false
 	}
-	return data, nil
+	nextPuzzleLetter := (*puzzle)[position[1]+direction[1]][position[0]+direction[0]]
+	nextXmasLetter := "XMAS"[xmasPosition[letter]+1]
+	validNextLetter := nextPuzzleLetter == nextXmasLetter
+	if !validNextLetter {
+		return false
+	}
+
+	if nextPuzzleLetter == 'S' {
+		return true
+	}
+	return validSubstr(direction, [2]int{position[0] + direction[0], position[1] + direction[1]}, puzzle)
 }
 
 func main() {
-	data, err := parseInput("/Users/samtang/Programming/advent-of-code-20204/day2/input.txt")
-	if err != nil {
-		return
+	puzzle := parseInput("/Users/samtang/Programming/advent-of-code-20204/day4/input.txt")
+	answer := 0
+	for y, line := range puzzle {
+		for x, character := range line {
+			if character == 'X' {
+				for _, direction := range directions {
+					if validSubstr(direction, [2]int{x, y}, &puzzle) {
+						answer += 1
+					}
+				}
+			}
+		}
 	}
-	fmt.Print(data)
+	fmt.Print(answer)
 }

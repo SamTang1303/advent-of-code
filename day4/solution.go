22ad8a2ae9cdfd3a36824fb65d12597e2d6baebf
SamTang1303 <samtang1303@gmail.com>
2024-12-10 15:56:19 -0800

    Day 4 part 2 solution and day 5

--- day4/solution.go
+++ day4/solution.go
@@ -54,17 +54,38 @@ func validSubstr(direction [2]int, position [2]int, puzzle *[]string) bool {
 	return validSubstr(direction, [2]int{position[0] + direction[0], position[1] + direction[1]}, puzzle)
 }
 
+func validXmas(position [2]int, puzzle *[]string) bool {
+	validX := position[0] > 0 && position[0] < len((*puzzle)[0])-1
+	validY := position[1] > 0 && position[1] < len(*puzzle)-1
+	if !validX || !validY {
+		return false
+	}
+	topLeft := (*puzzle)[position[1]-1][position[0]-1]
+	topRight := (*puzzle)[position[1]-1][position[0]+1]
+	bottomRight := (*puzzle)[position[1]+1][position[0]+1]
+	bottomLeft := (*puzzle)[position[1]+1][position[0]-1]
+
+	if !((bottomLeft == 'M' && topRight == 'S') || (bottomLeft == 'S' && topRight == 'M')) {
+		return false
+	}
+
+	if !((topLeft == 'M' && bottomRight == 'S') || (topLeft == 'S' && bottomRight == 'M')) {
+		return false
+	}
+	print(string(topLeft), " . ", string(topRight), "\n")
+	print(". ", string((*puzzle)[position[1]][position[0]]), " .", "\n")
+	print(string(bottomLeft), " . ", string(bottomRight), "\n")
+
+	return true
+}
+
 func main() {
 	puzzle := parseInput("/Users/samtang/Programming/advent-of-code-20204/day4/input.txt")
 	answer := 0
 	for y, line := range puzzle {
 		for x, character := range line {
-			if character == 'X' {
-				for _, direction := range directions {
-					if validSubstr(direction, [2]int{x, y}, &puzzle) {
-						answer += 1
-					}
-				}
+			if character == 'A' && validXmas([2]int{x, y}, &puzzle) {
+				answer += 1
 			}
 		}
 	}

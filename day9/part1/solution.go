413052e8975f9f851760115329dd2259d33aa38b
SamTang1303 <samtang1303@gmail.com>
2025-01-14 15:13:48 -0800

    Saving work before trying easy but slow solution for day 9

--- day9/part1/solution.go
+++ day9/part1/solution.go
@@ -24,22 +24,64 @@ func setCwdToSourceFile() {
 	}
 }
 
-func parseInput(filePath string) ([]string, error) {
+func parseInput(filePath string) (string, error) {
 	input, err := os.ReadFile(filePath)
 	if err != nil {
-		return []string{""}, fmt.Errorf("Problem reading file: %w", err)
+		return "", fmt.Errorf("Problem reading file: %w", err)
 	}
 	inputStr := strings.TrimSpace(string(input))
-	lines := strings.Split(inputStr, "\n")
-	return lines, nil
+	return inputStr, nil
 }
 
-func main() {
+func nTriangle(n int) int {
+	return n * (n + 1) / 2
+}
+
+func fileValue(index, size, location int) int {
+	id := index / 2
+	return id * ((location-1)*size + nTriangle(size))
+}
+
+func firstPass(input *string) ([][2]int, int) {
+	memory := make([][2]int, len(*input))
+	partialAns := 0
+	place := 0
+	for i, char := range *input {
+		toInt := int(char - '0')
+		memory[i] = [2]int{toInt, place}
+		if i%2 == 0 {
+			test := fileValue(i, toInt, place)
+			partialAns += test
+		}
+		place += toInt
+	}
+	return memory, partialAns
+}
+
+func secondPass(memory *[][2]int) int {
+	leftPointer := 1
+	rightPointer := len(*memory) - 1
+	partialAns := 0
+	if rightPointer%2 == 0 {
+		rightPointer -= 1
+	}
+	for leftPointer <= rightPointer {
+		id := rightPointer / 2
+		size := min((*memory)[leftPointer][0], (*memory)[rightPointer][0])
+	}
+	return partialAns
+}
+
+func part1() {
 	setCwdToSourceFile()
 	input, err := parseInput("../input")
 	if err != nil {
 		fmt.Print("Error parsing input: ", err)
 		return
 	}
-	fmt.Print(input[len(input)-1])
+	fmt.Print(firstPass(&input))
+}
+
+func main() {
+	part1()
 }
